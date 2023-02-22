e01953c114bc76aaabe648535038e3ed80032a39
cass <76407605+cleclair71@users.noreply.github.com>
2023-02-22 16:29:14 -0500

    cleanup

--- Develop/index.js
+++ Develop/index.js
@@ -1,130 +1,3 @@
-// <a name="readme-top"></a>
-
-// <br />
-// <div align="center">
-//   <a href="">
-//     <img src="" alt="Logo" width="200" height="200">
-//   </a>
-
-//   <h3 align="center">title</h3>
-
-//   <p align="center">
-//     tagline
-//   </p>
-// </div>
-
-
-
-// <!-- TABLE OF CONTENTS -->
-// <details>
-//   <summary>Table of Contents</summary>
-//   <ol>
-//     <li>
-//       <a href="#about-the-project">About The Project</a>
-//       <ul>
-//         <li><a href="#built-with">Built With</a></li>
-//       </ul>
-//     </li>
-//     <li>
-//       <a href="#getting-started">Getting Started</a>
-//     </li>
-//     <li><a href="#usage">Usage</a></li>
-//     <li><a href="#features">Features</a></li>
-//     <li><a href="#contributing">Contributing</a></li>
-//     <li><a href="#license">License</a></li>
-//     <li><a href="#contact">Contact</a></li>
-//     <li><a href="#acknowledgments">Acknowledgments</a></li>
-//   </ol>
-// </details>
-
-
-
-// <!-- ABOUT THE PROJECT -->
-// ## About The Project
-
-// example screenshot
-// ![screenshot](https://user-images./screenshot.JPG)
-
-// This is the description section of the readme. [Insert project name] is a [brief project description]. It was created to [state the project's purpose and goals]. This repository is where we [describe what's included in the repository, such as the source code, documentation, or other assets].
-
-// <p align="right">(<a href="#readme-top">back to top</a>)</p>
-
-
-
-// ### Built With
-
-// list of frameworks, libraries, and other tools used in the project
-// * [example](https://www.example.com/)
-
-
-// <p align="right">(<a href="#readme-top">back to top</a>)</p>
-
-
-// ## Getting Started
-
-//[Provide instructions on how to set up and run the project, including prerequisites, installation steps, and any other necessary details.]
-
-
-// <p align="right">(<a href="#readme-top">back to top</a>)</p>
-
-
-// ## Usage
-// [Provide examples or instructions on how to use the project, such as command-line arguments or API endpoints. Include screenshots or GIFs if applicable.]
-// ![examplegif](https://example.gif)
-
-// 1. example instructions
-//```sh
-// git clone https://github.com/your_username_/Project-Name.git
-// ```
-// 2. Install NPM 
-// ```sh
-// npm install
-// ```
-// <p align="right">(<a href="#readme-top">back to top</a>)</p>
-
-
-// ## Features
-
-// - [x] List the key features of the project
-// - [x] Additional feature if applicable
-
-
-// <p align="right">(<a href="#readme-top">back to top</a>)</p>
-
-
-// <!-- CONTRIBUTING -->
-// ## Contributing
-
-// [Explain how others can contribute to the project, such as by reporting bugs, suggesting new features, or submitting code changes. Include guidelines for pull requests and any other relevant information.]
-
-
-
-// <p align="right">(<a href="#readme-top">back to top</a>)</p>
-
-
-
-// <!-- LICENSE -->
-// ## License
-
-// [Specify the license under which the project is released, along with any relevant terms and conditions.]
-
-// <p align="right">(<a href="#readme-top">back to top</a>)</p>
-
-
-// ## Contact
-
-// [Provide contact information for the project maintainer or team, including email addresses or social media links, if applicable.]
-
-// <p align="right">(<a href="#readme-top">back to top</a>)</p>
-
-
-
-// <!-- ACKNOWLEDGMENTS -->
-// ## Acknowledgments
-
-// * [Resources you used](resourcesyouusedexample.com)
-
-
 
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
@@ -132,7 +5,6 @@ const path = require('path');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// const api = require('./utils/api.js');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -162,12 +34,6 @@ const questions = [
     },
     {
         type: 'input',
-        message: "List the frameworks, libraries, or other tools used in the project (separate with commas).",
-        name: 'builtWith',
-        default: 'Built with',
-    },
-    {
-        type: 'input',
         message: "Provide instructions on how to set up and run the project for the Installation section.",
         name: 'installation',
     },
@@ -189,18 +55,13 @@ const questions = [
     },
     {
         type: 'input',
-        message: "What resources did you use to create this project? (separate with commas)",
-        name: 'resources',
-    },
-    {
-        type: 'input',
         message: "What is the link to your repository?",
         name: 'repo',
         default: 'repo',
     },
     {
         type: 'input',
-        message: "What is your email",
+        message: "What is your email?",
         name: 'email',
         default: 'email',
     },
@@ -214,51 +75,17 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    // const builtWithSection = data.builtWith.join('\n');
-    // const featuresSection = data.features.map((feature) => `- [x] ${feature}`).join('\n');
-    // data.templateMarkdown = data.templateMarkdown
-    //   .replace('{{builtWith}}', builtWithSection)
-    //   .replace('{{features}}', featuresSection);
      return fs.writeFile(path.join (process.cwd(), fileName), data); 
   }
-//   const writeFileA = util.promisify(writeToFile);
+
 
   // TODO: Create a function to initialize app
   function init() {
     inquirer.prompt(questions).then((answers) => {
         console.log ("Generating markdown...");
         writeToFile('README.md', generateMarkdown(...answers));
-    //   const frameworks = answers.builtWith.split(',').map((fw) => fw.trim());
-    //   const data = {
-    //     username: answers.username,
-    //     repo: answers.repo,
-    //     title: answers.title,
-    //     tagline: answers.tagline,
-    //     description: answers.description,
-    //     builtWith: frameworks,
-    //     features: answers.features.split(',').map((feature) => feature.trim()),
-    //     gettingStarted: answers.gettingStarted,
-    //     usage: answers.usage,
-    //     contributing: answers.contributing,
-    //     license: answers.license,
-    //     templateMarkdown: templateMarkdown,
-    //   };
-     
     });
   }
   
   // Function call to initialize app
-  init() {
-     //api
-    //  const githubUser = await api.getUser(data.username);
-    //  console.log("Your Github user info: ", githubUser.data);
-    //    data.profileImage = githubUser.data.avatar_url;
-    //    console.log("Your profile image: ", data.profileImage);
-
-    //    console.log ("Generating markdown...");
-    //    const markdown = generateMarkdown(data);
-    //    console.log(markdown);
-    //    console.log("Writing to file...");
-    //    await
-    //  writeFileA('README.md', data);
-  };
+  init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -30,123 +30,108 @@ function renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `
   ${renderLicenseBadge(data.license)}
- // generate table of contents bases on responses
-  let tabCon = ### Table of Contents;
-
- * [Getting Started](#gettingStarted) 
-
-  * [Usage](#usage) 
-
- * [Contributing](#contributing) 
-
- * ${renderLicenseLink(data.license)}
-
-
-
- // readMe template
- <a name="readme-top"></a>
-
+  <a name="readme-top"></a>
  <br />
+ # ${data.title}
  <div align="center">
     <a href="">
      <img src="" alt="Logo" width="200" height="200">
    </a>
-    <h1 align="center">${data.title}</h1>
     <p align="center">
       ${data.tagline}
     </p>
   </div>
 
-  ## About The Project
+### Table of Contents;
 
-  ![screenshot](https://user-imagesexample./screenshot.JPG)
-
-  ${data.description}
-
-  <p align="right">(<a href="#readme-top">back to top</a>)</p>
-
-  
-  // add table of contents to template
-
-
-  // add built with to template
-  
-  ### Built With
-  ${data.builtWith}
-  
-  <p align="right">(<a href="#readme-top">back to top</a>)</p>
-  
-
-  
-  ## Installation
-  ${data.installation}
-
-  <p align="right">(<a href="#readme-top">back to top</a>)</p>
-  
-  // add usage to template
-  
-  ## Usage
-  ${data.usage}
-
-  <p align="right">(<a href="#readme-top">back to top</a>)</p>
-  
-
+* [About the Project](#description)) 
  
-     ## Contributing
+* [Installation](#installation)) 
+ 
+* [Usage](#usage) 
 
-     Thank you for your interest in contributing to this project! By contributing, you can help make this project even better for everyone.
+* [Contributing](#contributing)
 
-### How to Contribute
+* [Tests](#tests)
 
-      If you would like to contribute to this project, please follow these steps:
-      
-      1. **Fork the Project:** Fork this project to your own GitHub account.
-
-      2. **Create your Feature Branch:** Create a branch for your feature using git checkout -b your-branch-name.
-
-      3. **Commit your Changes:** Commit your changes using git commit -m "Your commit message here".
-
-      4. **Push to the Branch:** Push your changes to the branch using git push origin your-branch-name.
-
-      5. **Open a Pull Request:** Open a pull request to merge your changes into the main branch of the original project.
-
-     ### Contributors
-
-     We would like to extend our heartfelt thanks to the following individuals who have contributed to this project:
-
- * ${data.contributing}
-
-     If you have contributed to this project and your name is not on this list, please let us know so that we can add you. Your contributions are greatly appreciated!
+* [Questions](#email)
+ 
+* ${renderLicenseLink(data.license)}
 
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
 
+## About The Project
 
-    ## Resources
-  ${data.resources}
+![screenshot](https://user-imagesexample./screenshot.JPG)
 
-  <p align="right">(<a href="#readme-top">back to top</a>)</p>
+${data.description}
 
-  ${renderLicenseSection(data.license)}
-  ${data.license}
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## Installation
+
+${data.installation}
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
   
-  <p align="right">(<a href="#readme-top">back to top</a>)</p>
+## Usage
+  ${data.usage}
 
-  ## Tests
-  ${data.tests}
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+  
 
-  <p align="right">(<a href="#readme-top">back to top</a>)</p>
+ 
+## Contributing
+
+Thank you for your interest in contributing to this project! By contributing, you can help make this project even better for everyone.
+
+### How to Contribute
+
+If you would like to contribute to this project, please follow these steps:
+      
+1. **Fork the Project:** Fork this project to your own GitHub account.
+
+2. **Create your Feature Branch:** Create a branch for your feature using git checkout -b your-branch-name.
+
+3. **Commit your Changes:** Commit your changes using git commit -m "Your commit message here".
+
+4. **Push to the Branch:** Push your changes to the branch using git push origin your-branch-name.
+
+5. **Open a Pull Request:** Open a pull request to merge your changes into the main branch of the original project.
+
+### Contributors
+
+We would like to extend our heartfelt thanks to the following individuals who have contributed to this project:
+
+* ${data.contributing}
+
+If you have contributed to this project and your name is not on this list, please let us know so that we can add you. Your contributions are greatly appreciated!
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+${renderLicenseSection(data.license)}
+${data.license}
+  
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## Tests
+
+${data.tests}
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
 
 ## Questions?
 
-    If you have any questions about this project, please don't hesitate to reach out to me. I'm always happy to help and provide additional information.
+If you have any questions about this project, please don't hesitate to reach out to me. I'm always happy to help and provide additional information.
 
-    ### How to Ask a Question
-    1. **Check the existing documentation:** Before asking a question, please take a moment to review the existing documentation. Your question may already be answered in the README file or other project documentation.
+### How to Ask a Question
+
+1. **Check the existing documentation:** Before asking a question, please take a moment to review the existing documentation. Your question may already be answered in the README file or other project documentation.
 
 2. **Create a new issue:** If you can't find the answer to your question in the documentation, please create a new issue in the project's issue tracker. Be sure to provide as much detail as possible about your question, including any relevant code snippets or screenshots.
+
 [My Repository](#${data.repo})
 [My Github](#${data.username})
 
@@ -155,16 +140,9 @@ function generateMarkdown(data) {
 ### Feedback and Suggestions
 
 I welcome feedback and suggestions for this project. If you have ideas for how to improve the project, please feel free to create a new issue or contact me directly. Your input is greatly appreciated!
-
-
-
-
-
-  
+ 
   <p align="right">(<a href="#readme-top">back to top</a>)</p>
  
-
-
 `;
 }
 
